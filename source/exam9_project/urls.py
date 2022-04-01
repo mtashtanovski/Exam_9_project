922184a00d2958afd381445fa1b1c11d15a490c6
mtashtanovski <tashtanov.m@mail.ru>
2022-04-01 18:09:05 +0600

    1. Created first test view. 2. Tested main index page.

--- source/exam9_project/urls.py
+++ source/exam9_project/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webapp.urls')),
     path('api/', include(api_urlpatterns)),
-    path('accounts', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from django.views.generic import RedirectView
+
+from webapp.views.views import (
+    index_view,
+)
 
 app_name = 'webapp'
 
 urlpatterns = [
-
+    path('', index_view, name='index')
 ]
