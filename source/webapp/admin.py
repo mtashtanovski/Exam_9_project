709ed56552ab1d0fbce97afb3a5058db57270527
mtashtanovski <tashtanov.m@mail.ru>
2022-04-02 12:13:26 +0600

    1. Installed Pillow 2. Created models of Picture and Album. 3. Registered in admin pannel. 4. Made migrations and migrated. 5. Added media directory path.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Picture, Album
+
+
+class PictureAdmin(admin.ModelAdmin):
+    list_display = ['image', 'signature', 'created_at']
+    readonly_fields = ['created_at', 'author', 'album']
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'created_at']
+    readonly_fields = ['created_at', 'author']
+
+
+admin.site.register(Picture, PictureAdmin)
+admin.site.register(Album, AlbumAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,89 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Picture(models.Model):
+    image = models.ImageField(
+        verbose_name="Фото",
+        upload_to="media/",
+        null=False,
+        blank=False,
+    )
+    signature = models.TextField(
+        max_length=200,
+        verbose_name="Подпись",
+        null=False,
+        blank=False,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания",
+    )
+    author = models.ForeignKey(
+        User,
+        related_name="pictures",
+        on_delete=models.CASCADE,
+        default=1,
+        verbose_name="Автор",
+        null=False,
+    )
+    album = models.ForeignKey(
+        'webapp.Album',
+        on_delete=models.CASCADE,
+        related_name='pictures',
+        verbose_name="Альбом",
+        null=True,
+        blank=True,
+    )
+    is_private = models.BooleanField(
+        null=False,
+        blank=False,
+        default=False,
+        verbose_name="Приватный",
+    )
+
+    def __str__(self):
+        return f"{self.image} {self.signature}"
+
+    class Meta:
+        verbose_name = 'Фотография'
+        verbose_name_plural = 'Фотографии'
+
+
+class Album(models.Model):
+    title = models.CharField(
+        null=False,
+        blank=False,
+        max_length=50,
+        verbose_name="Название"
+    )
+    description = models.TextField(
+        max_length=300,
+        verbose_name="Описание",
+    )
+    author = models.ForeignKey(
+        User,
+        related_name="albums",
+        on_delete=models.CASCADE,
+        default=1,
+        verbose_name="Автор",
+        null=False,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания",
+    )
+    is_private = models.BooleanField(
+        null=False,
+        blank=False,
+        default=False,
+        verbose_name="Приватный"
+    )
+
+    def __str__(self):
+        return f"{self.title} {self.description}"
+
+    class Meta:
+        verbose_name = 'Альбом'
+        verbose_name_plural = 'Альбомы'
